2eec356c1d68cf1d9d826d94f747a7e2d2e31bab
Dabsia <dabojohnson98@gmail.com>
2022-02-17 14:57:56 +0000

    Took the user input upon form submission and also used 2-way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,98 @@
-import React from 'react'
-
+import React, {useState} from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+
+
+    // Using one state t handle all the userInputs
+
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount : '',
+    //     enteredDate: ''
+    // })
+
+    // const titleChangeHandler = (event) => {
+
+    //     // setUserInput({
+            
+    //     //     ...userInput,
+    //     //     enteredTitle: event.target.value,
+            
+    //     // })
+    //     setUserInput((prevState) => {
+    //         return {...prevState, enteredTitle:event.target.value}
+    //     })
+    // }
+
+    // const amountChangeHandler = (event) => {
+    //     setUserInput({
+            
+    //         ...userInput,
+    //         enteredAmount: event.target.value,
+            
+    //     })
+    // }
+    // const dateChangeHandler = (event) => {
+    //     setUserInput({
+            
+    //         ...userInput,
+    //         enteredDate: event.target.value,
+            
+    //     })
+    // }
+    // Always initialize all states to a string
+
+    // Initially the state set to an empty string
+    const [enteredTitle, setEnteredTitle] = useState('');
+
+    // Initially the amount state = ''
+    const [enteredAmount, setEnteredAmount] = useState('')
+
+    // Inititally the date state = ''
+    const [enteredDate, setEnteredDate] = useState('')
+
+    // This function should always react whenever the title input event changes
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value)
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
+    // To submit the form
+    const submitHandler = (e) =>{
+        e.preventDefault()
+        const expenseData = {
+            title:enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        } 
+        console.log(expenseData)
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className = 'new-expense__control'>
                     <label>Title</label>
-                    <input type = 'text'  />
+                    <input type = 'text' value = {enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className = 'new-expense__control'>
                     <label>Amount</label>
-                    <input type = 'number' min = '0.01' step = '0.01' />
+                    <input type = 'number' min = '0.01' step = '0.01' value = {enteredAmount} onChange={amountChangeHandler} />
                 </div>
                 <div className = 'new-expense__control'>
                     <label>Date</label>
-                    <input type = 'date' min = '2019-01-01' max = '2022-12-31' />
+                    <input type = 'date' min = '2019-01-01' max = '2022-12-31' value = {enteredDate}
+                    onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className='new-expense__actions'>
